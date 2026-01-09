ebbdaafd2f129243a2d53f1c201c97590704bb9a
jyi0226 <andy0372@naver.com>
2026-01-09 13:29:59 +0900

    feat: add routes for pages

--- src/shared/layout/header.tsx
+++ src/shared/layout/header.tsx
@@ -1,24 +1,34 @@
+import { NavLink } from 'react-router-dom';
+
 export function Header() {
   return (
     <header className="w-full bg-black text-white px-6 py-3">
       <div className="mx-auto max-w-7xl">
-        <div className="flex items-center justify-between gap-8">
+        <nav className="flex items-center justify-between gap-8">
           {/*Logo*/}
           <div className="shrink-0">
             <img src="/logo2.svg" alt="SSCC Logo" className="h-8 w-auto" />
           </div>
 
           {/*Menu*/}
-          {/* 추후 링크 연결*/}
-          <a className="text-[11px] font-semibold text-gray-300 shrink-0">메인</a>
-          <a className="text-[11px] font-semibold text-gray-300 shrink-0">소개</a>
-          <a className="text-[11px] font-semibold text-gray-300 shrink-0">프로젝트</a>
+          <NavLink to="/" className="text-[11px] font-semibold text-gray-300 shrink-0">
+            메인
+          </NavLink>
+          <NavLink to="/about" className="text-[11px] font-semibold text-gray-300 shrink-0">
+            소개
+          </NavLink>
+          <NavLink to="/projects" className="text-[11px] font-semibold text-gray-300 shrink-0">
+            프로젝트
+          </NavLink>
 
           {/*Apply*/}
-          <a className="shrink-0 rounded-full border border-teal-300/80 px-[12px] py-2 text-[11px] font-semibold text-teal-200 hover:bg-teal-300/10">
+          <NavLink
+            to="/apply"
+            className="shrink-0 rounded-full border border-teal-300/80 px-[12px] py-2 text-[11px] font-semibold text-teal-200 hover:bg-teal-300/10"
+          >
             지원하기
-          </a>
-        </div>
+          </NavLink>
+        </nav>
       </div>
     </header>
   );
