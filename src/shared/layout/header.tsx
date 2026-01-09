045298b71f25af40e404b11c619d660d868f53cc
YoungJin Won <101448204+AI-WonYJ@users.noreply.github.com>
2026-01-10 01:32:05 +0900

    Revert "feat: add header layout"
    This reverts commit a99c5a641e54a0dfc1c902ed9f8aaad8a91665f2.

--- src/shared/layout/header.tsx
+++ src/shared/layout/header.tsx
@@ -1,29 +1,7 @@
 export function Header() {
   return (
-    <header className="w-full bg-black text-white px-6 py-3">
-      <div className="mx-auto flex max-w-7xl w-full justify-between items-center">
-        {/* Left: logo*/}
-        <div className="w-12 h-8 overflow-hidden">
-          <img
-            src="/logo.svg"
-            alt="SSCC Logo"
-            className="h-8 w-15 max-w-none object-none translate-x-0"
-          />
-        </div>
-
-        {/* Center: menu*/}
-        {/* 추후 링크 연결*/}
-        <nav className="flex items-center gap-11 text-[11px] font-semibold text-gray-300">
-          <a>메인</a>
-          <a>소개</a>
-          <a>프로젝트</a>
-        </nav>
-
-        {/* Right: apply*/}
-        <a className="rounded-full border border-teal-300/80 px-[12px] py-2 text-[11px] font-semibold text-teal-200 hover:bg-teal-300/10">
-          지원하기
-        </a>
-      </div>
+    <header className="w-full border-b p-4">
+      <div className="text-base font-semibold">SSCC</div>
     </header>
   );
 }
