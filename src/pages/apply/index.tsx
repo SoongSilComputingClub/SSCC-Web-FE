4a6211e6cee55146c8e10c87eabcaee2e59980c3
chaerishme <sbk93@naver.com>
2026-01-11 17:32:32 +0900

    feat: implement apply schedule section

--- src/pages/apply/index.tsx
+++ src/pages/apply/index.tsx
@@ -1,9 +1,13 @@
 import HeroSection from "./sections/hero-section";
+import TargetSection from "./sections/target-section";
+import ScheduleSection from "./sections/schedule-section";
 
 export default function ApplyPage() {
   return (
     <div>
       <HeroSection />
+      <TargetSection />
+      <ScheduleSection />
     </div>
   );
 }
