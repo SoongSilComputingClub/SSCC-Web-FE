11ad3facd07b08f8ef1996c20ed29413dd65db07
jyi0226 <andy0372@naver.com>
2026-01-11 20:34:02 +0900

    refactor: 공통 색상 적용, 헤더 내용 수정

--- src/shared/layout/header.tsx
+++ src/shared/layout/header.tsx
@@ -2,29 +2,25 @@ import { NavLink } from 'react-router-dom';
 
 export function Header() {
   return (
-    <header className="w-full bg-black text-white px-6 py-3">
+    <header className="w-full bg-bg-default text-text-default px-6 py-3">
       <div className="mx-auto max-w-7xl">
-        <nav className="flex items-center justify-between gap-8">
+        <nav className="flex items-center justify-between">
           {/*Logo*/}
-          <div className="shrink-0">
+          <NavLink to="/" className="shrink-0">
             <img src="/logo2.svg" alt="SSCC Logo" className="h-8 w-auto" />
-          </div>
-
-          {/*Menu*/}
-          <NavLink to="/" className="text-[11px] font-semibold text-gray-300 shrink-0">
-            메인
           </NavLink>
-          <NavLink to="/about" className="text-[11px] font-semibold text-gray-300 shrink-0">
+
+          <NavLink to="/about" className="text-[11px] font-semibold text-text-default shrink-0">
             소개
           </NavLink>
-          <NavLink to="/projects" className="text-[11px] font-semibold text-gray-300 shrink-0">
+          <NavLink to="/projects" className="text-[11px] font-semibold text-text-default shrink-0">
             프로젝트
           </NavLink>
 
           {/*Apply*/}
           <NavLink
             to="/apply"
-            className="shrink-0 rounded-full border border-teal-300/80 px-[12px] py-2 text-[11px] font-semibold text-teal-200 hover:bg-teal-300/10"
+            className="shrink-0 rounded-full border-[1.5px] border-point px-[12px] py-2 text-[11px] font-semibold text-point"
           >
             지원하기
           </NavLink>
