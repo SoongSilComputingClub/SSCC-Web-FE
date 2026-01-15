ebf919d557dd407bd97496827ddcc0097df2eb32
jyi0226 <andy0372@naver.com>
2026-01-15 17:58:58 +0900

    feat: compose apply page sections

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,16 @@
+import IntroSection from './sections/introduce-section';
+import ParallaxSection from './sections/scroll-section';
+import SubSection from './sections/semititle-section';
+import ScrollSlideSection from './sections/slide-section';
+import WaveSection from './sections/wave-section';
 export default function HomePage() {
-  return <div className="p-4">Home Page</div>;
+  return (
+    <div>
+      <WaveSection />
+      <SubSection />
+      <ScrollSlideSection />
+      <ParallaxSection />
+      <IntroSection />
+    </div>
+  );
 }
