5462763cb0071e095e211e10d5f87f796a5f3cb8
chaerishme <sbk93@naver.com>
2026-01-19 01:16:18 +0900

    chore: update global config for browser environment

--- eslint.config.js
+++ eslint.config.js
@@ -3,17 +3,27 @@ import tseslint from 'typescript-eslint';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import prettier from 'eslint-plugin-prettier';
+import globals from 'globals';
 
-export default tseslint.config(js.configs.recommended, ...tseslint.configs.recommended, {
-  plugins: {
-    'react-hooks': reactHooks,
-    'react-refresh': reactRefresh,
-    prettier,
+export default tseslint.config(
+  {
+    ignores: ['**/node_modules/**', '**/dist/**', '**/.vite/**'],
   },
-  rules: {
-    ...reactHooks.configs.recommended.rules,
-    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-    'prettier/prettier': 'error',
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'prettier/prettier': 'error',
+    },
   },
-  ignorePatterns: ["node_modules/", "dist/", ".vite/"],
-});
+);
