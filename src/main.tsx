d19a5bc5a83e90cccf5d40b00b257c7afdfbc31d
jyi0226 <andy0372@naver.com>
2026-01-23 01:58:17 +0900

    [Refactor(#8)] named import로 변경

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,20 @@
-import React from 'react';
+import { StrictMode } from 'react';
 
 import { QueryClientProvider } from '@tanstack/react-query';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-//import { registerSW } from 'virtual:pwa-register';
 
 import './styles/globals.css';
 
 import { queryClient } from './app/queryClient';
 import { router } from './app/router';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
     </QueryClientProvider>
-  </React.StrictMode>,
+  </StrictMode>,
 );
 
 //registerSW({ immediate: true });
