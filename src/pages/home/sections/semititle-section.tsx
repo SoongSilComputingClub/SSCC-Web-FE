0424f5ae0feb560f3f4f3fb94b3629abe9a6db94
jyi0226 <andy0372@naver.com>
2026-01-23 00:50:37 +0900

    [Feat(#8)]: semititle 섹션 구현

--- src/pages/home/sections/semititle-section.tsx
+++ src/pages/home/sections/semititle-section.tsx
@@ -1,9 +1,9 @@
 export default function SubSection() {
   return (
     <section className="w-full pointer-events-none">
-      <div className="bg-bg-default flex flex-col items-center px-6 text-center">
+      <div className="pt-10 bg-bg-default flex flex-col px-3 items-center text-center">
         {/* Subtitle */}
-        <p className="text-2xl font-semibold text-text-default ">코딩의 시작 SSCC를 소개합니다</p>
+        <p className="text-[25px] text-text-default ">코딩의 시작 SSCC를 소개합니다</p>
 
         {/* Divider */}
         <div className="mt-12 w-full h-px bg-text-default/50" />
