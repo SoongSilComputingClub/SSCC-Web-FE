a50c8de5ee59f708ef4147ad1eb365d10dce0397
YoungJin Won <101448204+AI-WonYJ@users.noreply.github.com>
2026-01-23 05:47:45 +0000

    Revert "[Refactor(#8)] named import로 변경"
    This reverts commit a26425830d95138055a253fdc3a3fa6e96052c3f.

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,21 @@
-import { StrictMode } from 'react';
+import React from 'react';
 
 import { QueryClientProvider } from '@tanstack/react-query';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+//import { registerSW } from 'virtual:pwa-register';
 
 import './styles/globals.css';
 
 import { queryClient } from './app/queryClient';
 import { router } from './app/router';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
     </QueryClientProvider>
-  </StrictMode>,
+  </React.StrictMode>,
 );
 
 //registerSW({ immediate: true });
