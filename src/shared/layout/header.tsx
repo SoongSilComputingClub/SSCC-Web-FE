a88202ce7f5ae94cd91efdc772eae6ecf4c5c966
jyi0226 <andy0372@naver.com>
2026-02-03 16:55:00 +0900

    [DESIGN(#35)] 헤더를 통한 페이지 이동 시 최상단으로 이동

--- src/shared/layout/header.tsx
+++ src/shared/layout/header.tsx
@@ -6,20 +6,33 @@ export function Header() {
       <div className="mx-auto max-w-7xl">
         <nav className="flex items-center justify-between">
           {/*Logo*/}
-          <NavLink to="/" className="shrink-0">
+          <NavLink
+            to="/"
+            onClick={() => window.scrollTo({ top: 0, left: 0, behavior: 'auto' })}
+            className="shrink-0"
+          >
             <img src="/logo2.svg" alt="SSCC Logo" className="h-8 w-auto" />
           </NavLink>
 
-          <NavLink to="/about" className="shrink-0 text-[11px] font-semibold text-text-default">
+          <NavLink
+            to="/about"
+            onClick={() => window.scrollTo({ top: 0, left: 0, behavior: 'auto' })}
+            className="shrink-0 text-[11px] font-semibold text-text-default"
+          >
             소개
           </NavLink>
-          <NavLink to="/projects" className="shrink-0 text-[11px] font-semibold text-text-default">
+          <NavLink
+            to="/projects"
+            onClick={() => window.scrollTo({ top: 0, left: 0, behavior: 'auto' })}
+            className="shrink-0 text-[11px] font-semibold text-text-default"
+          >
             프로젝트
           </NavLink>
 
           {/*Apply*/}
           <NavLink
             to="/apply"
+            onClick={() => window.scrollTo({ top: 0, left: 0, behavior: 'auto' })}
             className="shrink-0 rounded-full border-[1.5px] border-point px-[12px] py-2 text-[11px] font-semibold text-point"
           >
             지원하기
