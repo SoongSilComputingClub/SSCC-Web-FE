dc3b0734e336c6eb28a2d58ad9a0d53794e5d949
Youngin Jeong <105589421+jyi0226@users.noreply.github.com>
2026-02-03 21:27:37 +0900

    [CHORE(#36)] 헤더 고정 컴포넌트 구현
    * [DESIGN(#35)] 헤더 디자인 고정 컴포넌트화
    
    * [DESIGN(#35)] 헤더를 통한 페이지 이동 시 최상단으로 이동
    
    * [DESIGN(#35)] 지원하기 클릭 시 지원 페이지 최상단 이동
    
    * [FEAT(#35)] 라우트 이동 발생시 스크롤 최상단으로 이동
    
    onClick이 아닌 ScrollToTop 사용하여 이동 후 점프처럼 보이는 문제 해결
    
    * [RENAME(#35)] 파일명 kebal-case 변경
    
    * [CHORE(#35)] import 경로 수정

--- src/shared/layout/app-shell.tsx
+++ src/shared/layout/app-shell.tsx
@@ -2,10 +2,11 @@ import { Outlet } from 'react-router-dom';
 
 import { Footer } from './footer';
 import { Header } from './header';
-
+import { ScrollToTop } from './scroll-to-top';
 export function AppShell() {
   return (
     <div className="flex min-h-screen flex-col">
+      <ScrollToTop />
       <Header />
       <main className="flex-1">
         <Outlet />
