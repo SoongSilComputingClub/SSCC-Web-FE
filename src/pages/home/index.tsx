7e3228f3b30cb6d91db68b22f6eaadefe1df9318
jyi0226 <andy0372@naver.com>
2026-02-18 16:04:55 +0900

    [DESIGN(#69)] semititle 삭제

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,11 @@
 import IntroSection from './sections/introduce-section';
 import ParallaxSection from './sections/scroll-section';
-import SubSection from './sections/semititle-section';
 import ScrollSlideSection from './sections/slide-section';
 import { WaveSection } from './sections/wave-section';
 export default function HomePage() {
   return (
     <div>
       <WaveSection />
-      <SubSection />
       <ScrollSlideSection />
       <ParallaxSection />
       <IntroSection />
